c24fe3a4baad7aacdd8796746b8a984d5036e3f5
dlwl98 <ff981113@hanyang.ac.kr>
2022-09-04 21:32:09 +0900

    쿠키, 세션을 이용한 로그인 구현

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -61,6 +61,8 @@ export const postLogin = async (req, res) => {
     });
   }
   // Login Success
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
@@ -16,6 +19,16 @@ app.set("views", process.cwd() + "/src/views");
 // use Middlewares
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+
+app.use(localsMiddleware);
 
 // use Routers
 app.use("/", rootRouter);
