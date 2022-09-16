ccb4cefc21265eec4070287e56dc4ddb642af141
dlwl98 <ff981113@hanyang.ac.kr>
2022-09-16 17:32:26 +0900

    video grid css & webpack config

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,5 +1,21 @@
 import "../scss/styles.scss";
 
+const videoResize = () => {
+  const videoGrid = document.querySelector(".video-grid");
+  if (videoGrid) {
+    const videoNum = Math.floor(window.innerWidth / 300) + 1;
+    videoGrid.style.gridTemplateColumns = `repeat(${videoNum}, minmax(0, 1fr))`;
+
+    const videoThumb = document.querySelectorAll(".video-mixin__thumb");
+    if (videoThumb[0]) {
+      const width = videoThumb[0].getBoundingClientRect().width;
+      for (let i = 0; i < videoThumb.length; i++) {
+        videoThumb[i].style.height = `${(width * 10) / 16}px`;
+      }
+    }
+  }
+};
+
 // 파일업로드 처리
 const filebox = document.querySelector(".filebox");
 if (filebox) {
@@ -9,3 +25,7 @@ if (filebox) {
     fileInput[0].placeholder = file.name;
   });
 }
+
+// 썸네일 반응형 크기조절
+videoResize();
+window.addEventListener("resize", videoResize);
