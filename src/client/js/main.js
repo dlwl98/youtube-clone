5e90ed0095ed0702163fb341c30be6c47e86b31c
dlwl98 <ff981113@hanyang.ac.kr>
2022-09-16 16:08:52 +0900

    css 마무리

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,6 +1,10 @@
 import "../scss/styles.scss";
 
-const hello = () => {
-  console.log("hi");
-};
-hello();
+// 파일업로드 처리
+const filebox = document.querySelector(".filebox");
+const fileInput = filebox.getElementsByClassName("input__filename");
+const fileLabelInput = filebox.getElementsByClassName("input__file");
+filebox.addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  fileInput[0].placeholder = file.name;
+});
