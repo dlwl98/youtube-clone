4b840e8f3e265b8aa972cb822a3cd0c7b91c0d03
dlwl98 <ff981113@hanyang.ac.kr>
2022-09-20 19:48:56 +0900

    재생컨트롤러 스타일

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,28 +1,28 @@
 import "../scss/styles.scss";
 
 const videoResize = () => {
-  const videoGrid = document.querySelector(".video-grid");
-  if (videoGrid) {
+  const $videoGrid = document.getElementById("video-grid");
+  if ($videoGrid) {
     const videoNum = Math.floor(window.innerWidth / 300) + 1;
-    videoGrid.style.gridTemplateColumns = `repeat(${videoNum}, minmax(0, 1fr))`;
+    $videoGrid.style.gridTemplateColumns = `repeat(${videoNum}, minmax(0, 1fr))`;
 
-    const videoThumb = document.querySelectorAll(".video-mixin__thumb");
-    if (videoThumb[0]) {
-      const width = videoThumb[0].getBoundingClientRect().width;
-      for (let i = 0; i < videoThumb.length; i++) {
-        videoThumb[i].style.height = `${(width * 10) / 16}px`;
+    const $videoThumb = document.getElementsByClassName("video-mixin__thumb");
+    if ($videoThumb[0]) {
+      const width = $videoThumb[0].getBoundingClientRect().width;
+      for (let i = 0; i < $videoThumb.length; i++) {
+        $videoThumb[i].style.height = `${(width * 10) / 16}px`;
       }
     }
   }
 };
 
 // 파일업로드 처리
-const filebox = document.querySelector(".filebox");
-if (filebox) {
-  const fileInput = filebox.getElementsByClassName("input__filename");
-  filebox.addEventListener("change", (e) => {
-    const file = e.target.files[0];
-    fileInput[0].placeholder = file.name;
+const $filebox = document.getElementsByClassName("filebox")[0];
+if ($filebox) {
+  const $fileInput = $filebox.getElementsByClassName("input__filename")[0];
+  $filebox.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    $fileInput.placeholder = file.name;
   });
 }
 
--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,158 @@
-console.log("watch");
+const $volumeRangeVisible = document.getElementById("volume__range-visible");
+const $volumeRangeInvisible = document.getElementById(
+  "volume__range-invisible"
+);
+const $timeRangeVisible = document.getElementById("time__range-visible");
+const $timeRangeInvisible = document.getElementById("time__range-invisible");
+const $mainVideo = document.getElementById("main-video");
+const $playBtn = document.getElementById("play-btn");
+const $playBtnIcon = $playBtn.querySelector("i");
+const $muteBtn = document.getElementById("mute-btn");
+const $muteBtnIcon = $muteBtn.querySelector("i");
+const $fullscreenBtn = document.getElementById("fullscreen-btn");
+const $fullscreenBtnIcon = $fullscreenBtn.querySelector("i");
+const $videoContainer = document.getElementById("video__container");
+const $currentTime = document.getElementById("current-time");
+const $totalTime = document.getElementById("total-time");
+const $videoControls = document.getElementById("video__controls");
+
+const formatTime = (seconds) => {
+  let start = 11;
+  if (seconds < 600) start = 15;
+  else if (seconds > 3600) start = 12;
+  else start = 14;
+
+  return new Date(seconds * 1000).toISOString().substring(start, 19);
+};
+
+// 비디오 컨트롤러
+let volumeValue = 0.5;
+$mainVideo.volume = volumeValue;
+
+const handlePlayBtn = (event) => {
+  if ($mainVideo.paused) {
+    $mainVideo.play();
+  } else {
+    $mainVideo.pause();
+  }
+  $playBtnIcon.classList = $mainVideo.paused ? "fas fa-play" : "fas fa-pause";
+};
+
+const handleMuteBtn = (event) => {
+  $mainVideo.muted = !$mainVideo.muted;
+  $muteBtnIcon.classList = $mainVideo.muted
+    ? "fas fa-volume-mute"
+    : "fas fa-volume-up";
+  $volumeRangeInvisible.value = $volumeRangeVisible.value = $mainVideo.muted
+    ? 0
+    : volumeValue;
+};
+
+$playBtn.addEventListener("click", handlePlayBtn);
+$muteBtn.addEventListener("click", handleMuteBtn);
+
+// 소리조절 바
+const handleVolumeChange = (value) => {
+  $mainVideo.muted = value === "0" ? true : false;
+  $muteBtnIcon.classList = $mainVideo.muted
+    ? "fas fa-volume-mute"
+    : "fas fa-volume-up";
+  volumeValue = value;
+  $mainVideo.volume = value;
+};
+
+const handleVolumeVisible = (event) => {
+  const {
+    target: { value },
+  } = event;
+  $volumeRangeInvisible.value = value;
+  handleVolumeChange(value);
+};
+
+const handleVolumeInvisible = (event) => {
+  const {
+    target: { value },
+  } = event;
+  $volumeRangeVisible.value = value;
+  handleVolumeChange(value);
+};
+
+$volumeRangeVisible.oninput = handleVolumeVisible;
+$volumeRangeVisible.onchange = handleVolumeVisible;
+$volumeRangeInvisible.oninput = handleVolumeInvisible;
+$volumeRangeInvisible.onchange = handleVolumeInvisible;
+
+// 시간 최신화
+const handleLoaded = () => {
+  $currentTime.innerText = formatTime(0);
+  $totalTime.innerText = formatTime(Math.floor($mainVideo.duration * 10) / 10);
+  $timeRangeVisible.max = $timeRangeInvisible.max =
+    Math.floor($mainVideo.duration * 10) / 10;
+};
+const handleTimeupdate = () => {
+  $currentTime.innerText = formatTime(
+    Math.floor($mainVideo.currentTime * 10) / 10
+  );
+  $timeRangeVisible.value = $timeRangeInvisible.value =
+    Math.floor($mainVideo.currentTime * 10) / 10;
+};
+const handleTimeVisible = (event) => {
+  const {
+    target: { value },
+  } = event;
+  $timeRangeInvisible.value = value;
+  $mainVideo.currentTime = value;
+};
+const handleTimeInvisible = (event) => {
+  const {
+    target: { value },
+  } = event;
+  $timeRangeVisible.value = value;
+  $mainVideo.currentTime = value;
+};
+
+$mainVideo.addEventListener("loadedmetadata", handleLoaded);
+$mainVideo.addEventListener("timeupdate", handleTimeupdate);
+$timeRangeVisible.oninput = handleTimeVisible;
+$timeRangeVisible.onchange = handleTimeVisible;
+$timeRangeInvisible.oninput = handleTimeInvisible;
+$timeRangeInvisible.onchange = handleTimeInvisible;
+
+// 풀스크린
+const handleFullscreenBtn = () => {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+    $fullscreenBtnIcon.classList = "fas fa-expand";
+  } else {
+    $videoContainer.requestFullscreen();
+    $fullscreenBtnIcon.classList = "fas fa-compress";
+  }
+};
+const handleFullscreenChange = () => {
+  if (!document.fullscreenElement) {
+    $fullscreenBtnIcon.classList = "fas fa-expand";
+  }
+};
+$fullscreenBtn.addEventListener("click", handleFullscreenBtn);
+$videoContainer.addEventListener("fullscreenchange", handleFullscreenChange);
+
+// 컨트롤러 사라지게
+let controlsTimeout = null;
+
+const handleMouseMove = () => {
+  $videoControls.classList.remove("hide");
+  if (controlsTimeout) {
+    clearTimeout(controlsTimeout);
+    controlsTimeout = null;
+  }
+  controlsTimeout = setTimeout(() => {
+    $videoControls.classList.add("hide");
+  }, 3000);
+};
+
+const handleMouseLeave = () => {
+  $videoControls.classList.add("hide");
+};
+
+$videoContainer.addEventListener("mousemove", handleMouseMove);
+$videoContainer.addEventListener("mouseleave", handleMouseLeave);
